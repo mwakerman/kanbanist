dfe48ef3430a8f08e919f8d44564b5c8b532c8c2
Misha Wakerman <m.wakerman@gmail.com>
2019-04-01 00:28:10 +1100

    remove issue from known issues page

--- src/pages/Issues.js
+++ src/pages/Issues.js
@@ -18,22 +18,6 @@ export default () => (
                 </em>
                 .
             </p>
-            <h4>ISSUE 02: Quick add tasks.</h4>
-            <p>
-                Kanbanist allows you to use Todoist{' '}
-                <a href="https://support.todoist.com/hc/en-us/articles/115001745265-Task-Quick-Add">
-                    Quick Add syntax{' '}
-                </a>
-                when creating new tasks (note: this is not possible when editing existing tasks). Unlike the regular
-                Todoist client, projects must be quick added without spaces. For example, if your project is called
-                "Reading List", then you quick add a task by inserting "#readinglist". Also, because Kanbanist uses a
-                more complete markdown converter, putting your project at the beginning of a task will show it (briefly)
-                as a heading. To not see this, put your project syntax within or at the end of your item. For example,
-                type "thing to do #project" rather than "#project thing to do".
-            </p>
-            <h4>
-                <strike>ISSUE 03: Quick-Add fails when project name contains emojis.</strike>
-            </h4>
             <h4>ISSUE 04: General support for Todoist Filters</h4>
             <p>
                 I've received a number of requests for supporting Todoist Filters in various ways. Most commonly to
