d658e62874a2eafec2f2c47a229eb0171fd22cbd
Misha Wakerman <misha@atticus.tech>
2020-02-27 19:25:20 +1100

    Prevent list creation unless user has Todoist Premium.

--- src/redux/modules/lists.js
+++ src/redux/modules/lists.js
@@ -488,7 +488,10 @@ function setSortBy(state, action) {
 function moveItem(state, action) {
     const { itemId, fromListId, toListId, itemIndex } = action.payload;
 
-    const item = state.lists.push(state.backlog).find(l => l.id === fromListId).get('items').find(i => i.id === itemId);
+    const item = state.lists.push(state.backlog)
+        .find(l => l.id === fromListId).get('items')
+        .find(i => i.id === itemId);
+
     const updatedLists = state.lists.map(l => {
         let newList = l;
 
