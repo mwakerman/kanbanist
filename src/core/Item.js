44061a8d00078807c1d21daca5f3f23f60456c16
Misha Wakerman <misha@atticus.tech>
2020-06-08 12:34:42 +1000

    Issue #93: Allow filtering by sub-tasks.

--- src/core/Item.js
+++ src/core/Item.js
@@ -8,13 +8,14 @@ const ItemRecord = Record({
     due_date_utc: '',
     recurring: false,
     priority: 0,
+    parent_id: null,
     responsible_uid: null,
     date_added: '',
     date_string: '',
 });
 
 export default class Item extends ItemRecord {
-    updateWith({ id, text, item_order, project, due, priority, responsible_uid, date_added, date_string }) {
+    updateWith({ id, text, item_order, project, due, priority, parent_id, responsible_uid, date_added, date_string }) {
         return new Item({
             id: id || this.id,
             text: text || this.text,
@@ -23,6 +24,7 @@ export default class Item extends ItemRecord {
             due_date_utc: due ? due.date : this.due_date_utc,
             recurring: (due && due.is_recurring) || this.recurring,
             priority: priority || this.priority,
+            parent_id: parent_id || this.parent_id,
             responsible_uid: responsible_uid || this.responsible_uid,
             date_added: date_added || this.date_added,
             date_string: date_string || this.date_string,
